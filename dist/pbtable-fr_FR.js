ad5d10322e84a98011f451dc91c6c3caa28b57a6
ryinhard <ryinhard@gmail.com>
2018-12-28 16:40:04 +0800

    overwrite index html
    覆蓋首頁

---
+++ dist/pbtable-fr_FR.js
@@ -0,0 +1,14 @@
+jQuery.extend( jQuery.fn.pbTable.defaults, {
+  locale:{
+    btnView:'Vue',
+    btnEdit:'Mettre à jour',
+    btnDelete:'Effacer',
+    btnNew:'Nouveau',
+    btnPrevious:'Page précédent',
+    btnNext:'Page suivante',
+    btnFirst:'Première page',
+    btnLast:'Dernière page',
+    placeholderSearchBox:'Recherche...',
+    msgNoData:'Aucun résultat trouvé'
+  }
+}); //$.fn.pbTable.defaults
---
+++ dist/pbtable-it_IT.js
@@ -0,0 +1,14 @@
+jQuery.extend( jQuery.fn.pbTable.defaults, {
+  locale:{
+    btnView:'Vedere',
+    btnEdit:'Editare',
+    btnDelete:'Anulla',
+    btnNew:'Nuovo',
+    btnPrevious:'Precedente',
+    btnNext:'Prossimo',
+    btnFirst:'Prima paggina',
+    btnLast:'Ultima paggina',
+    placeholderSearchBox:'Ricerca...',
+    msgNoData:'Non si travorono risultati'
+  }
+}); //$.fn.pbTable.defaults
